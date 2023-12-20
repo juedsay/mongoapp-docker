80195dd1e8971cfa7b814a85363af20c107a8b3b
Julian Simon <32204826+juedsay@users.noreply.github.com>
2023-12-20 12:26:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const Animal = mongoose.model('Animal', new mongoose.Schema({
 
 const app = express()
 
-mongoose.connect('mongodb://nico:password@monguito:27017/miapp?authSource=admin')
+mongoose.connect('mongodb://juedsay:password@mongoMC:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
-  console.log('listando... chanchitos...')
+  console.log('listando... perros...')
   const animales = await Animal.find();
   return res.send(animales)
 })
 app.get('/crear', async (_req, res) => {
   console.log('creando...')
-  await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' })
+  await Animal.create({ tipo: 'Perro', estado: 'Peludito' })
   return res.send('ok')
 })
 
